a4868af8c16f0c51c7ade06c172fa9338bafda02
staroon <staroon@outlook.com>
2021-01-09 12:05:09 +0800

    presto 332 udfs init

---
+++ README.md
@@ -0,0 +1,35 @@
+### Presto SQL UDFS
+Presto SQL 自定义函数    
+
+### 项目环境
+- Presto SQL: 332
+- Java: 1.8.0_161
+- Maven: 3.6.2
+
+### 函数列表
+#### 标量函数（Scalar Function）
+- **ip_location**(mode, ip) -> varchar    
+    - 函数描述    
+    IP 定位函数，依赖于网络开源的 IP 数据库，如无必要，不会频繁更新，预计每半年更新一次，如果查询返回值为 0 ，说明 IP 位置信息未收录
+    - 参数说明    
+        - mode，查询模式，字符串类型，支持参数：country（国家）、province（省份）、city（城市）、isp（网络运营商）    
+        - ip，IP 地址，字符串类型，格式如：xxx.xxx.xxx.xxx    
+    - 使用示例
+    ```sql
+    select 
+      ip_location('country','112.126.60.145') "国家",
+      ip_location('province','112.126.60.145') "省份",
+      ip_location('city','112.126.60.145') "城市",
+      ip_location('isp','112.126.60.145') "网络运营商",
+      ip_location('city','112.'), -- 非正常 IP ，返回空白字符串
+      ip_location('city',null), -- IP 值为 null ，返回空白字符串
+      ip_location('city','36.98.202.245') -- 返回值为 0 ，说明未收录该 IP 该项位置信息
+    ;
+    ```
+
+### 使用说明
+1. 克隆项目到本地，`git clone project`    
+2. 编译项目，`mvn clean package`    
+3. 得到插件 zip 包：`presto-udfs-332.zip`    
+4. 解压 zip 文件至 Presto SQL 安装目录 **plugin** 目录下    
+5. 重启 Presto SQL    
---
+++ src/main/java/com/staroon/bigdata/presto/udfs/scalar/IPLocationFunction.java
@@ -0,0 +1,113 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.staroon.bigdata.presto.udfs.scalar;
+
+import io.airlift.slice.Slice;
+import io.airlift.slice.Slices;
+import io.prestosql.spi.PrestoException;
+import io.prestosql.spi.function.Description;
+import io.prestosql.spi.function.ScalarFunction;
+import io.prestosql.spi.function.SqlNullable;
+import io.prestosql.spi.function.SqlType;
+import io.prestosql.spi.type.StandardTypes;
+import org.lionsoul.ip2region.DbConfig;
+import org.lionsoul.ip2region.DbMakerConfigException;
+import org.lionsoul.ip2region.DbSearcher;
+import org.lionsoul.ip2region.Util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static io.prestosql.spi.StandardErrorCode.COMPILER_ERROR;
+import static io.prestosql.spi.StandardErrorCode.INVALID_ARGUMENTS;
+
+/**
+ * @author Staroon Created on 2020/12/31 17:54
+ * @version 1.0
+ */
+public class IPLocationFunction
+{
+    private static final DbSearcher SEARCHER;
+
+    static {
+        String filePath = new IPLocationFunction().getJarHome();
+        String dbFile = filePath + "/ip2region.db";
+        try {
+            DbConfig config = new DbConfig();
+            SEARCHER = new DbSearcher(config, dbFile);
+        }
+        catch (DbMakerConfigException e) {
+            throw new PrestoException(COMPILER_ERROR, "ip2region config init failed.");
+        }
+        catch (FileNotFoundException e) {
+            throw new PrestoException(COMPILER_ERROR, "ip2region db file not find.");
+        }
+    }
+
+    private IPLocationFunction() {}
+
+    @ScalarFunction("ip_location")
+    @Description("Return the input ip location: country, province, city, isp")
+    @SqlType(StandardTypes.VARCHAR)
+    public static Slice ipToRegion(@SqlType(StandardTypes.VARCHAR) Slice mode, @SqlNullable @SqlType(StandardTypes.VARCHAR) Slice ipSlice)
+    {
+        if (ipSlice == null) {
+            return Slices.EMPTY_SLICE;
+        }
+
+        String ip = ipSlice.toStringUtf8();
+        String locations;
+
+        if (Util.isIpAddress(ip)) {
+            try {
+                locations = SEARCHER.memorySearch(ip).getRegion();
+            }
+            catch (Exception e) {
+                return Slices.EMPTY_SLICE;
+            }
+        }
+        else {
+            return Slices.EMPTY_SLICE;
+        }
+
+        String[] res = locations.split("\\|");
+        if (res.length < 5) {
+            return Slices.utf8Slice(locations);
+        }
+        String result;
+        switch (mode.toStringUtf8()) {
+            case "country":
+                result = res[0];
+                break;
+            case "province":
+                result = res[2];
+                break;
+            case "city":
+                result = res[3];
+                break;
+            case "isp":
+                result = res[4];
+                break;
+            default:
+                throw new PrestoException(INVALID_ARGUMENTS, "Invalid arguments, the param1 could be: country, province, city, isp.");
+        }
+        return Slices.utf8Slice(result);
+    }
+
+    private String getJarHome()
+    {
+        return new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).getParent();
+    }
+}
